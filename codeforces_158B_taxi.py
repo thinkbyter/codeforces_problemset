62b876437007eb80ce962dbf3aeee6fea1befe25
bugfoot <19329416+bugfoot@users.noreply.github.com>
2021-02-04 06:06:31 +0100

    Added solution to https://codeforces.com/problemset/problem/158/B.

---
+++ codeforces_158B_taxi.py
@@ -0,0 +1,35 @@
+# https://codeforces.com/problemset/problem/158/B
+# Difficulty: 1100
+# Trials: 6
+# Time: 00:45:14
+
+n = int(input())
+
+r = 0
+d = {i: 0 for i in range(1, 4)}
+for s in map(int, input().split()):
+  if s == 4:
+    r += 1
+  else:
+    d[s] += 1
+
+
+r += d[2] // 2
+d[2] %= 2
+
+if d[2] == 1:
+  r += 1
+  if d[1] >= 2:
+    d[1] -= 2
+  if d[1] == 1 and d[3] == 0:
+    d[1] -= 1
+
+r += d[3]
+d[1] -= min(d[1], d[3])
+
+r += d[1] // 4
+d[1] %= 4
+if d[1] > 0:
+  r += 1
+
+print(r)
