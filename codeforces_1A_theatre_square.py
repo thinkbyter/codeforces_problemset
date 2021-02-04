a562fd34d859fb50a964652974f24b7640ab672b
bugfoot <19329416+bugfoot@users.noreply.github.com>
2021-02-04 05:08:12 +0100

    Added solution to https://codeforces.com/problemset/problem/1/A.

---
+++ codeforces_1A_theatre_square.py
@@ -0,0 +1,8 @@
+# https://codeforces.com/problemset/problem/1/A
+# Difficulty: 900
+# Trials: 1
+# Time: 00:03:33
+
+n, m, a = map(int, input().split())
+
+print((n // a + (0 if n % a == 0 else 1)) * (m // a + (0 if m % a == 0 else 1)))
