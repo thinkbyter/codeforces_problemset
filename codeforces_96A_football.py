af2399bb2d11b490dbde1007a10656d31bb084b2
bugfoot <19329416+bugfoot@users.noreply.github.com>
2021-02-04 04:58:50 +0100

    Added solution to https://codeforces.com/problemset/problem/96/A.

---
+++ codeforces_96A_football.py
@@ -0,0 +1,21 @@
+# https://codeforces.com/problemset/problem/96/A
+# Difficulty: 900
+# Trials: 3
+# Time: 00:09:53
+
+ss = input()
+prev = ss[0]
+count = 1
+msg = "NO"
+for s in ss[1:]:
+  if s == prev:
+    count += 1
+  else:
+    count = 1
+    prev = s
+    
+  if count >= 7:
+    msg = "YES"
+    break
+    
+print(msg)
